056357d123015ce3828351da8d94571136b010b6
harshil-gandhi-16 <harshil.gandhi@logicwind.com>
2024-07-17 14:00:15 +0530

    Initial commit

---
+++ Example/AppsOnAirIOSCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppsOnAirIOSCore
+//
+//  Created by harshil-gandhi-16 on 07/17/2024.
+//  Copyright (c) 2024 harshil-gandhi-16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
